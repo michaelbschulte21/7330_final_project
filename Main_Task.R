20d79bc955763b7b97efffab1b1d6345057dd1b9
michaelbschulte21 <87950696+michaelbschulte21@users.noreply.github.com>
2023-02-23 19:48:41 -0600

    Checks to see if formula1 schema exists before running create script.

--- Main_Task.R
+++ Main_Task.R
@@ -20,7 +20,21 @@ drop_all_schema <- function(){
   print('All existing schemas have been nuked')
 }
 
-source('Schema_Creator_formula1.R')
+source('Connections/Local_connect.R')
+rm(secrets)
+
+script <- paste0("SELECT SCHEMA_NAME
+                    FROM INFORMATION_SCHEMA.SCHEMATA
+                    WHERE SCHEMA_NAME = 'formula1'")
+flag <- dbGetQuery(conn = dbconnection_local, statement = script)
+
+flag <- nrow(flag) == 0
+
+dbKillConnections()
+
+if(flag){
+  source('Schema_Creator_formula1.R')
+}
 
 source('API/API_access_pt1.R')
 
