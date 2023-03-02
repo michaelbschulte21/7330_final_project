df67402d3094f4cfac68dbf4aeb9baaea8f0f595
michaelbschulte21 <87950696+michaelbschulte21@users.noreply.github.com>
2023-03-01 20:54:55 -0600

    Created guidelines for setting up SQL

---
+++ secrets_guide.R
@@ -0,0 +1,6 @@
+# secrets.R
+
+secrets <- data.frame(local_server = 'localhost',
+                      local_port = 3306,
+                      local_uid = 'root',
+                      local_pwd = 'password')
