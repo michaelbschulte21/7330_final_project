6160e58b571581b415aeb7ad01e44d0c25eb69f5
michaelbschulte21 <87950696+michaelbschulte21@users.noreply.github.com>
2023-03-01 22:22:10 -0600

    EDA folder created

---
+++ EDA/Test.R
@@ -0,0 +1,19 @@
+# EDA/Test.R
+
+source('Connections/Master_connect.R')
+rm(secrets)
+
+get_year <- 2013
+
+script <- paste0("SELECT *
+                 FROM races
+                 WHERE year = ", get_year, ";")
+
+get_year <- c(2013, 1998, 2015)
+
+script <- paste0("SELECT *
+                 FROM races
+                 WHERE ", paste("year = ", get_year, "", sep = "", collapse = "\n OR "), ";")
+# dbExecute(conn, statement) Probably won't use this
+df <- dbGetQuery(conn = dbconnection_master, statement = script)
+
