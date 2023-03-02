044d303abc87c7a8d203e52d91547506d218d5aa
michaelbschulte21 <87950696+michaelbschulte21@users.noreply.github.com>
2023-03-01 21:40:27 -0600

    Added trycatch to libraries to install packages if loading them fails.

--- Libraries.R
+++ Libraries.R
@@ -1,11 +1,24 @@
 # Libraries.R
 
-library(odbc)
-library(RSQLite)
-library(RMySQL)
-library(tidyverse)
-library(DBI)
-library(sqldf)
-library(httr)
-library(jsonlite)
-library(lubridate)
+tryCatch({
+  library(odbc)
+  library(RSQLite)
+  library(RMySQL)
+  library(tidyverse)
+  library(DBI)
+  library(sqldf)
+  library(httr)
+  library(jsonlite)
+  library(lubridate)
+}, error = function(e){
+  install.packages('odbc')
+  install.packages('RSQLite')
+  install.packages('RMySQL')
+  install.packages('tidyverse')
+  install.packages('DBI')
+  install.packages('sqldf')
+  install.packages('httr')
+  install.packages('jsonlite')
+  install.packages('lubridate')
+}
+)
