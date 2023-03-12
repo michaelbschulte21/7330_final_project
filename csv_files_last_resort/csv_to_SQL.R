042c82ee33d8bddd7fc6ef68fa02d4bf39fd8e5d
michaelbschulte21 <87950696+michaelbschulte21@users.noreply.github.com>
2023-03-11 22:00:37 -0600

    More annoying structure

--- csv_files_last_resort/csv_to_SQL.R
+++ csv_files_last_resort/csv_to_SQL.R
@@ -24,9 +24,27 @@ csv_to_SQL <- function(table_name){
 }
 
 dbSendQuery(conn = dbconnection_master, "SET GLOBAL local_infile = true;")
+tryCatch({
+  for(i in 1:length(f1_tables)){
+    csv_to_SQL(table_name = f1_tables[i])
+  }
+}, error = function(e){
+  
+  # basic_paste_structure_for_insert <- paste("(",,")", sep = "", collapse = ",\n")
+  
+  df <- data.frame(read.csv(file = paste0('csv_files_last_resort/', f1_tables[1], '.csv')), stringsAsFactors = FALSE)
+  script <- paste0("INSERT INTO circuits (circuit_ID, circuit_name, circuit_abbr, locality, country, latitude, longitude)
+                   VALUES ", paste("(",
+                                    df$circuit_ID, ", '",
+                                   df$circuit_name, "', '",
+                                   df$circuit_abbr, "', '",
+                                   df$locality, "', '",
+                                   df$country, "', ",
+                                   df$latitude, ", ",
+                                   df$longitude
+                                   ,")", sep = "", collapse = ",\n"),";")
+  dbExecute(conn = dbconnection_master, statement = script)
+})
 
-for(i in 1:length(f1_tables)){
-  csv_to_SQL(table_name = f1_tables[i])
-}
 
 dbKillConnections()
